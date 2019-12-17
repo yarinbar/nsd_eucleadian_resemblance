26ac1ccb85606eb90adca5b7a704a2689213e0e6
Yarin <yarinbar@campus.technion.ac.il>
2019-12-17 10:12:58 +0800

    added matrix to benching

--- bench.py
+++ bench.py
@@ -1,4 +1,5 @@
-import _cpp_dis
+import _matrix
+import cpp_dis
 import py_dis
 import random
 import time
@@ -7,27 +8,44 @@ if __name__ == '__main__':
 	
 	runs = 10000
 	
+	
+	
 	cpp_s_time = time.time()
 	
 	for i in range(runs):
-		vec_a = random.sample(range(4096), 1024)
-		vec_b = random.sample(range(4096), 1024)
-		dis = _cpp_dis.euclidean(vec_a, vec_b)
-	
+		A = _matrix.Matrix(1, 1024)
+		B = _matrix.Matrix(1, 1024)
+		
+		for j in range(1024):
+			A[1, j] = random.randint(-4096, 4096) / 1024
+			B[1, j] = random.randint(-4096, 4096) / 1024
+		
+		dis = cpp_dis.euclidean(A, B)
+		
 	cpp_e_time = time.time()
 	
 	cpp_time = cpp_e_time - cpp_s_time
 	
+	
+	
+	
 	py_s_time = time.time()
 	
 	for i in range(runs):
-		vec_a = random.sample(range(4096), 1024)
-		vec_b = random.sample(range(4096), 1024)
-		dis = py_dis.euclidean(vec_a, vec_b)
+		A = _matrix.Matrix(1, 1024)
+		B = _matrix.Matrix(1, 1024)
+		
+		for j in range(1024):
+			A[1, j] = random.randint(-4096, 4096) / 1024
+			B[1, j] = random.randint(-4096, 4096) / 1024
+		
+		dis = py_dis.euclidean(A, B)
 	
 	py_e_time = time.time()
 	
 	py_time = py_e_time - py_s_time
+	
+	
 	
 	print("py_time / cpp_time = {}".format(float(py_time / cpp_time)))
 	
