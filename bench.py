6e520a6b85cc9e475464d6e1c515f7a9c7445a8a
Yarin <yarinbar@campus.technion.ac.il>
2019-12-19 17:00:04 +0800

    acuurate benching

--- bench.py
+++ bench.py
@@ -17,9 +17,8 @@ if __name__ == '__main__':
     vlen = 1024
     A = matrix.Matrix(vlen, 1)
     B = matrix.Matrix(vlen, 1)
-    print("doing cpp")
-    cpp_s_time = time.time()
-
+    
+	print("doing cpp")
     for i in range(runs):
 
         for j in range(vlen):
@@ -27,53 +26,50 @@ if __name__ == '__main__':
 #            print(A[j, 0])
             B[j, 0] = random.randint(-4096, 4096) / 128
 #            print(B[j, 0])
-
+		cpp_s_time = time.time()
         dis = cpp_dis.euclidean(A, B)
-
-    cpp_e_time = time.time()
-    cpp_time = cpp_e_time - cpp_s_time
+		cpp_e_time = time.time()
+		cpp_time += cpp_e_time - cpp_s_time
+    
 	
     print("doing mkl")
-    mkl_s_time = time.time()
-
     for i in range(runs):
         for j in range(vlen):
             A[j, 0] = random.randint(-4096, 4096) / 128
 #            print(A[j, 0])
             B[j, 0] = random.randint(-4096, 4096) / 128
 #            print(B[j, 0])
+		mkl_s_time = time.time()
         dis = cpp_dis.mkl_euclidean(A, B)
-
-    mkl_e_time = time.time()
-    mkl_time = mkl_e_time - mkl_s_time
+		mkl_e_time = time.time()
+		mkl_time += mkl_e_time - mkl_s_time
 	
     print("doing py")
-    py_s_time = time.time()
-
     for i in range(runs):
         for j in range(vlen):
             A[j, 0] = random.randint(-4096, 4096) / 128
 #            print(A[j, 0])
             B[j, 0] = random.randint(-4096, 4096) / 128
 #            print(B[j, 0])
+		py_s_time = time.time()
         dis = py_dis.euclidean(A, B)
+		py_e_time = time.time()
+		py_time += py_e_time - py_s_time
 
-    py_e_time = time.time()
-    py_time = py_e_time - py_s_time
+    
 	
     print("doing numpy")
-    npy_s_time = time.time()
-
     for i in range(runs):
         for j in range(vlen):
             A = (np.random.rand(vlen, 1) - 0.5) * 64
 #            print(A[j, 0])
             B = (np.random.rand(vlen, 1) - 0.5) * 64
 #            print(B[j, 0])
+		npy_s_time = time.time()
         dis = py_dis.np_euclidean(A, B)
-
-    npy_e_time = time.time()
-    npy_time = npy_e_time - npy_s_time
+		npy_e_time = time.time()
+		npy_time += npy_e_time - npy_s_time
+    
 
     print("cpp_time / mkl_time  = {}".format(float(cpp_time / mkl_time)))
     print("py_time  / cpp_time  = {}".format(float(py_time  / cpp_time)))
