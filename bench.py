b1cc041a18c76bdfb4b145828c4a57610b302beb
Ubuntu <ubuntu@ip-172-31-90-243.ec2.internal>
2019-12-25 00:45:29 +0800

    benching is perfect

--- bench.py
+++ bench.py
@@ -18,26 +18,25 @@ if __name__ == '__main__':
     A = matrix.Matrix(vlen, 1)
     B = matrix.Matrix(vlen, 1)
 	
-	cpp_time = 0
+    cpp_time = 0
     mkl_time = 0
-	py_time = 0
-	npync_time = 0
-	npyc_time = 0
-	
-	print("doing cpp")
-    for i in range(runs):
+    py_time = 0
+    npync_time = 0
+    npyc_time = 0
 
+    print("doing cpp")
+    for i in range(runs):
         for j in range(vlen):
             A[j, 0] = random.randint(-4096, 4096) / 128
 #            print(A[j, 0])
             B[j, 0] = random.randint(-4096, 4096) / 128
 #            print(B[j, 0])
-		cpp_s_time = time.time()
+        cpp_s_time = time.time()
         dis = cpp_dis.euclidean(A, B)
-		cpp_e_time = time.time()
-		cpp_time += cpp_e_time - cpp_s_time
-    
-	
+        cpp_e_time = time.time()
+        cpp_time += cpp_e_time - cpp_s_time
+
+
     print("doing mkl")
     for i in range(runs):
         for j in range(vlen):
@@ -45,11 +44,11 @@ if __name__ == '__main__':
 #            print(A[j, 0])
             B[j, 0] = random.randint(-4096, 4096) / 128
 #            print(B[j, 0])
-		mkl_s_time = time.time()
+        mkl_s_time = time.time()
         dis = cpp_dis.mkl_euclidean(A, B)
-		mkl_e_time = time.time()
-		mkl_time += mkl_e_time - mkl_s_time
-	
+        mkl_e_time = time.time()
+        mkl_time += mkl_e_time - mkl_s_time
+
     print("doing py")
     for i in range(runs):
         for j in range(vlen):
@@ -57,22 +56,22 @@ if __name__ == '__main__':
 #            print(A[j, 0])
             B[j, 0] = random.randint(-4096, 4096) / 128
 #            print(B[j, 0])
-		py_s_time = time.time()
+        py_s_time = time.time()
         dis = py_dis.euclidean(A, B)
-		py_e_time = time.time()
-		py_time += py_e_time - py_s_time
+        py_e_time = time.time()
+        py_time += py_e_time - py_s_time
 
-	print("doing numpy no conversion")
-	for i in range(runs):
+    print("doing numpy no conversion")
+    for i in range(runs):
         for j in range(vlen):
             A[j, 0] = random.randint(-4096, 4096) / 128
 #            print(A[j, 0])
             B[j, 0] = random.randint(-4096, 4096) / 128
 #            print(B[j, 0])
-		npync_s_time = time.time()
-        dis = py_dis.np_euclidean(A, B)
-		npync_e_time = time.time()
-		npync_time += npync_e_time - npync_s_time
+        npync_s_time = time.time()
+        dis = py_dis.np_euclidean(A.array, B.array)
+        npync_e_time = time.time()
+        npync_time += npync_e_time - npync_s_time
 	
     print("doing numpy conversion")
     for i in range(runs):
@@ -81,16 +80,15 @@ if __name__ == '__main__':
 #            print(A[j, 0])
             B = (np.random.rand(vlen, 1) - 0.5) * 64
 #            print(B[j, 0])
-		npyc_s_time = time.time()
+        npyc_s_time = time.time()
         dis = py_dis.np_euclidean(A, B)
-		npyc_e_time = time.time()
-		npyc_time += npyc_e_time - npyc_s_time
+        npyc_e_time = time.time()
+        npyc_time += npyc_e_time - npyc_s_time
     
 
     print("mkl_time / cpp_time  = {}".format(float(mkl_time / cpp_time)))
     print("py_time  / cpp_time  = {}".format(float(py_time  / cpp_time)))
     print("npync_time / cpp_time   = {}".format(float(npync_time / cpp_time)))
     print("npyc_time / cpp_time  = {}".format(float(npyc_time / cpp_time)))
-
     print("done")
 
